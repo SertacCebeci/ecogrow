6fcddf5e18caacf008183878dd05a8682f489f58
SertacCebeci <ali.sertac.cebeci.95@gmail.com>
2023-05-30 23:03:43 +0300

    fix: product page fix

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -19,15 +19,17 @@ const ProductDetails = () => {
   }, []);
 
   return (
-    <div className="md:w-2/6 w-4/6 h-5/6 flex flex-col items-center justify-start rounded-md border border-black">
+    <div className="md:w-2/6 w-4/6 h-5/6 flex flex-col items-center justify-start rounded-md">
       <div
         id="header"
         className="flex items-end justify-center w-full h-1/6 my-8"
       ></div>
 
       <div className="flex flex-col items-center justify-evenly w-full h-2/6 my-24">
-        <p className="text-2xl font-bold mb-4">Your Plant's Latest Stage</p>
-        <div>
+        <p className="text-xl md:text-2xl font-bold mb-4">
+          Your Plant's Latest Stage
+        </p>
+        <div className="m-4">
           <Image
             src={"data:image/gif;base64," + measurement["String Photo"]}
             width={400}
@@ -35,15 +37,29 @@ const ProductDetails = () => {
             alt="Picture of the author"
           />
         </div>
-        <div className="flex flex-row items-center justify-center w-full my-6">
-          <p className="font-bold text-xl mx-6">Ec: {measurement.ph}</p>
-          <p className="font-bold text-xl mx-6">pH: {measurement.Ec}</p>
+        <div className="flex flex-col md:flex-row  items-center justify-around my-6 w-9/12">
+          <p className="text-xl my-1">
+            <span className="font-bold ">Ec: </span>
+            <span className="font-bold text-green-900">
+              {measurement.Ec} ppm
+            </span>
+          </p>
+          <p className="text-xl my-1">
+            <span className="font-bold">pH: </span>
+            <span className="font-bold text-green-900">{measurement.ph}</span>
+          </p>
+          <p className="text-xl my-1">
+            <span className="font-bold">Water Height: </span>
+            <span className="font-bold text-green-900">
+              {1023 - measurement.height}
+            </span>
+          </p>
         </div>
         <div>
-          <p className="font-bold text-xl">{`Snapshot interval ${interval} minutes`}</p>
+          <p className="font-bold md:text-xl text-center">{`Snapshot interval ${interval} minutes`}</p>
         </div>
         <div>
-          <p className="font-bold text-xl">
+          <p className="font-bold md:text-xl text-normal text-center text-green-800">
             Plants have no NPK malnutrition detected
           </p>
         </div>
